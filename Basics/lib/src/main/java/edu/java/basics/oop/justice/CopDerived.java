70bdf7a78a82c642b0087089aa21fbaccbf32a25
jdhall01 <jeetendra.dhall.01@gmail.com>
2021-10-01 18:02:46 +0530

    Java Basics - OOP - super() and this() in constructor

--- Basics/lib/src/main/java/edu/java/basics/oop/justice/CopDerived.java
+++ Basics/lib/src/main/java/edu/java/basics/oop/justice/CopDerived.java
@@ -2,6 +2,17 @@ package edu.java.basics.oop.justice;
 
 public class CopDerived extends Cop {
 	
+	public CopDerived() {
+		//this(2);//ERROR: We cannot have both super() and this()
+		//the second one among them will give an error: Constructor call must be the first statement.
+		super();
+		
+	}
+	
+	public CopDerived(int a) {
+		
+	}
+	
 	//can access package private from derived class within the same package
 	public void accessBasePackagePrivate () {
 		super.gun2 = 5;
