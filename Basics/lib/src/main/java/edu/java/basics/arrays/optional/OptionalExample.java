5024447c7c12e96981274b2521c35fb689b5604d
jdhall01 <jeetendra.dhall.01@gmail.com>
2021-10-03 15:01:54 +0530

    Java Basics - Optional

---
+++ Basics/lib/src/main/java/edu/java/basics/arrays/optional/OptionalExample.java
@@ -0,0 +1,34 @@
+package edu.java.basics.arrays.optional;
+
+import java.util.Optional;
+
+public class OptionalExample {
+
+	protected static void println(Object o) {
+		System.out.println(o);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//empty Optional
+		Optional<String> emptyOpt = Optional.empty();
+		println(emptyOpt.isEmpty());
+		
+		//filled Optional
+		String str = "Hello";
+		Optional<String> filledOpt = Optional.of(str);
+		println(filledOpt.isPresent());
+		
+		//fill if null
+		String nullStr = null;//needed, else Typemismatch, cannot convert from Object to String
+		String strFillIfNull = Optional.ofNullable(nullStr).orElse("Hello");
+		println(strFillIfNull);
+		
+		//print if present
+		str = "Jeetu";
+		Optional<String> filledOpt2 = Optional.of(str);
+		filledOpt2.ifPresent(name -> println(name));
+	}
+
+}
