9b853927fb857975905b060b2238891c7e6a1389
jdhall01 <jeetendra.dhall.01@gmail.com>
2021-10-03 11:56:19 +0530

    Java Basics - Functional - good old anonymous class before lambda feature was introduced in Java 8

--- Basics/lib/src/main/java/edu/java/basics/functional/greetings/WellWisher.java
+++ Basics/lib/src/main/java/edu/java/basics/functional/greetings/WellWisher.java
@@ -1,7 +1,7 @@
 package edu.java.basics.functional.greetings;
 
 //https://www.educative.io/courses/java-8-lambdas-stream-api-beyond/JP2PrpzzjoK
-public class wellWisher {
+public class WellWisher {
 
 	public static void wish(Greeting greeting) {
 		greeting.greet();
@@ -13,9 +13,20 @@ public class wellWisher {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//lambda
 		wish(() -> System.out.println("Namaste!"));
+		
+		//incorrect lambda setup - wish2 must accept a functional interface in its definition
 		//ERROR:The target type of this expression must be a functional interface
 		//wish2(() -> System.out.println("Namaste!"));
+
+		//the good old anonymous class prior to lambda feature
+		wish(new Greeting() {
+			@Override
+			public void greet() {
+				System.out.println("Hello!");
+			}
+		});
 	}
 
 }
